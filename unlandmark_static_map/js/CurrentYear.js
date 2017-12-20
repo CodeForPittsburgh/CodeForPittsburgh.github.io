3621a51e12cd7f6e468cddda2040559a9f23f0f4
mhowe0422 <mhowe0422@msn.com>
2017-12-20 12:03:20 -0500

    updates

--- unlandmark_static_map/js/CurrentYear.js
+++
@@ -1,6 +0,0 @@
-function CurrentYear(id) {
-    var d = new Date();
-    var n = d.getFullYear();
-    document.getElementById(id).innerHTML = "Code For Pittsburgh © " + n;
-    //document.getElementById("Disclaimer").innerHTML = "Code For Pittsburgh © " + n;
-}
