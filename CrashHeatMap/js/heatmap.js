73b2a508fab185e22c080f25db6f8b0ebd100874
mhowe0422 <mhowe0422@msn.com>
2018-04-27 09:21:53 -0400

    Update change 3

--- CrashHeatMap/js/heatmap.js
+++ CrashHeatMap/js/heatmap.js
@@ -133,9 +133,20 @@ function toggleHeatmap() {
 function changeGradient() {
 
     var gradient = [
-        'rgba(0, 255, 255, 0)',
-        'rgba(0, 255, 255, 1)',
-        'rgba(0, 191, 255, 1)'
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
     ];
     heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
 }
