7178e6d3081c0c8691ebac4d20fbfcf62eb11822
mhowe0422 <mhowe0422@msn.com>
2019-09-11 11:17:19 -0400

    New Bootstarp code

--- BikeCrashMap/js/crashmap.js
+++ BikeCrashMap/js/crashmap.js
@@ -1,6 +1,10 @@
 //src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCb3EA0lfao273s6Jkp8tfTzJfUSkswpOw&libraries=visualization";
-src = "jquery-1.11.2.js";
-src = "bootstrap.js";
+src='https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"';
+src='"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"';
+src='"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"';
+
+//src = "jquery-1.11.2.js";
+//src = "bootstrap.js";
 src = "citymapoverlays.js";
 
 var state = {"map": null,
@@ -220,7 +224,7 @@ function initialize() {
 
 function bindInfoWindow(marker, map, infoWindow, html) {
 
-    google.maps.event.addListener(marker, 'click', function () {
+    google.maps.event.addListener(marker, 'click' || 'touchstart', function () {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
         //infoWindow.open(marker.get('map'), marker);
