ffbda44e03dae27b2c338ff3bff61a8b4a5a65aa
Max Chis <maxachis@gmail.com>
2022-01-05 17:54:32 -0500

    Additional updates

--- FoodAccessMap/script.js
+++ FoodAccessMap/script.js
@@ -274,6 +274,7 @@ var setSearchLocation = function( latlng ){
     $("#results").empty();
     for (var i = 0; i < Object.entries(foodLocations._layers).length; i++) {
       entry = Object.entries(foodLocations._layers)[i][1].feature.properties;
+      var entryDiv = $("<div class=\"entryDiv\"></div>")
       var heading = $("<h2></h2>").text(entry.name);
       var siteTypeText = entry.type;
       if (siteTypeText != null) {
@@ -288,37 +289,40 @@ var setSearchLocation = function( latlng ){
           href : googleMapsUrl,
           text : "Find On Google Maps"
       });
-      if (entry.SNAP == 1 || entry.WIC == 1 || entry.food_bucks == 1) {
-        $("#results").append("<p>Accepts</p>");        
-        var acceptsText = $("<ul></ul>");
+      entryDiv.append(heading);
+      entryDiv.append(siteType);
+      entryDiv.append(address);
+      entryDiv.append(googleMapsLink);
+      entryDiv.append($("<br>"));
+      if (entry.SNAP == 1 || entry.WIC == 1 || entry.food_bucks == 1) {   
+        var acceptsText = $("<div></div>");
         if (entry.SNAP == 1) {
-          acceptsText.append($("<li></li>").text("SNAP"));
+          acceptsText.append($("<span class=\"snap accepts_icon\"></span>").text("SNAP"));
         }
         if (entry.WIC == 1) {
-          acceptsText.append($("<li></li>").text("WIC"));
+          acceptsText.append($("<span class=\"wic accepts_icon\"></span>").text("WIC"));
+
         } 
         if (entry.food_bucks == 1) {
-          acceptsText.append($("<li></li>").text("Food Bucks"));
+          acceptsText.append($("<span class=\"foodbucks accepts_icon\"></span>").text("Food Bucks"));
         }
-        $("#results").append(acceptsText);
+        if (entry.fresh_produce == 1) {
+          acceptsText.append($("<span class=\"freshproduce accepts_icon\"></span>").text("Fresh Produce"));
+        }
+        if (entry.FMNP == 1) {
+          acceptsText.append($("<br>"));
+          acceptsText.append($("<span class=\"fmnp accepts_icon\"></span>").text("Farmer's Market Nutrition Program"));
+        }
+        entryDiv.append(acceptsText);
       }
 
 
-      if (entry.fresh_produce == 1) {
-        $("#results").append("<p>Offers fresh produce</p>");
-      }
-      if (entry.FMNP == 1) {
-        $("#results").append("<p>Part of Farmer's Market Nutrition Program</p>");
-      }
 
 
-      $("#results").append(heading);
-      $("#results").append(siteType);
-      $("#results").append(address);
-      $("#results").append(googleMapsLink);
+
+
+      $("#results").append(entryDiv)
       console.log(entry);
-      // .feature.properties;
-      // console.log(entry);
     }
     
 }
