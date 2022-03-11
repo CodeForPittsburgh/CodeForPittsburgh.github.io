97083f9b4b5ed0c5687b59a4437a29d975bec4f2
mhowe0422 <mhowe0422@msn.com>
2022-03-11 16:13:05 -0500

    Changed to handle current/future fish fry status - maybe

--- FishFryCountdown/js/CalculateEaster.js
+++ FishFryCountdown/js/CalculateEaster.js
@@ -1,8 +1,8 @@
 /**
-* Calculates Easter in the Gregorian/Western (Catholic and Protestant) calendar 
-* based on the algorithm by Oudin (1940) from http://www.tondering.dk/claus/cal/easter.php
-* @returns {array} [int month, int day]
-*/
+ * Calculates Easter in the Gregorian/Western (Catholic and Protestant) calendar 
+ * based on the algorithm by Oudin (1940) from http://www.tondering.dk/claus/cal/easter.php
+ * @returns {array} [int month, int day]
+ */
 var clock = "";
 var daysSpan = "";
 var hoursSpan = "";
@@ -10,124 +10,139 @@ var minutesSpan = "";
 var secondsSpan = "";
 var timeinterval = "";
 var targettime = "";
+var ashwednesday = "";
+var easter = "";
 
 function getEaster(year) {
-	var f = Math.floor,
-		// Golden Number - 1
-		G = year % 19,
-		C = f(year / 100),
-		// related to Epact
-		H = (C - f(C / 4) - f((8 * C + 13)/25) + 19 * G + 15) % 30,
-		// number of days from 21 March to the Paschal full moon
-		I = H - f(H/28) * (1 - f(29/(H + 1)) * f((21-G)/11)),
-		// weekday for the Paschal full moon
-		J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
-		// number of days from 21 March to the Sunday on or before the Paschal full moon
-		L = I - J,
-		month = 3 + f((L + 40)/44),
-		day = L + 28 - 31 * f(month / 4);
+    var f = Math.floor,
+            // Golden Number - 1
+            G = year % 19,
+            C = f(year / 100),
+            // related to Epact
+            H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30,
+            // number of days from 21 March to the Paschal full moon
+            I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11)),
+            // weekday for the Paschal full moon
+            J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
+            // number of days from 21 March to the Sunday on or before the Paschal full moon
+            L = I - J,
+            month = 3 + f((L + 40) / 44),
+            day = L + 28 - 31 * f(month / 4);
 
-	return [getMonth(month),day];
+    return [getMonth(month), day];
 }
 function getMonth(month)
 {
-    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    return months[month-1];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[month - 1];
 }
-  
+
 function getAshWednesday(year) {
-	var f = Math.floor,
-		// Golden Number - 1
-		G = year % 19,
-		C = f(year / 100),
-		// related to Epact
-		H = (C - f(C / 4) - f((8 * C + 13)/25) + 19 * G + 15) % 30,
-		// number of days from 21 March to the Paschal full moon
-		I = H - f(H/28) * (1 - f(29/(H + 1)) * f((21-G)/11)),
-		// weekday for the Paschal full moon
-		J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
-		// number of days from 21 March to the Sunday on or before the Paschal full moon
-		L = 80 + I - J - 46,
-		month = 1+f((L+40 )/44),
-                feb = 28 * (f(month/3)),
-                lyear = leapyear(year),
-		day = L + 7 -31 -feb + lyear;
+    var f = Math.floor,
+            // Golden Number - 1
+            G = year % 19,
+            C = f(year / 100),
+            // related to Epact
+            H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30,
+            // number of days from 21 March to the Paschal full moon
+            I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11)),
+            // weekday for the Paschal full moon
+            J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
+            // number of days from 21 March to the Sunday on or before the Paschal full moon
+            L = 80 + I - J - 46,
+            month = 1 + f((L + 40) / 44),
+            feb = 28 * (f(month / 3)),
+            lyear = leapyear(year),
+            day = L + 7 - 31 - feb + lyear;
 //var days = 31 ;
-	return [getMonth(month),day];
-}  
+    return [getMonth(month), day];
+}
 
 function getGoodFriday(year) {
-	var f = Math.floor,
-		// Golden Number - 1
-		G = year % 19,
-		C = f(year / 100),
-		// related to Epact
-		H = (C - f(C / 4) - f((8 * C + 13)/25) + 19 * G + 15) % 30,
-		// number of days from 21 March to the Paschal full moon
-		I = H - f(H/28) * (1 - f(29/(H + 1)) * f((21-G)/11)),
-		// weekday for the Paschal full moon
-		J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
-		// number of days from 21 March to the Sunday on or before the Paschal full moon
-		L = I - J,
-		month = 3 + f((L + 40)/44),
-		day = L + 26 - 31 * f(month / 4);
+    var f = Math.floor,
+            // Golden Number - 1
+            G = year % 19,
+            C = f(year / 100),
+            // related to Epact
+            H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30,
+            // number of days from 21 March to the Paschal full moon
+            I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11)),
+            // weekday for the Paschal full moon
+            J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
+            // number of days from 21 March to the Sunday on or before the Paschal full moon
+            L = I - J,
+            month = 3 + f((L + 40) / 44),
+            day = L + 26 - 31 * f(month / 4);
 //var days = 31 ;
-	return [getMonth(month),day];
-}  
+    return [getMonth(month), day];
+}
 
 function leapyear(year)
 {
-    
-if( (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0))
-{
-    return 1;
-}
-else
-{
-    return 0;
-}
-}
-function getTimeRemaining(targettime) {
-    var t = Date.parse(targettime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
 
-
-
-function initializeFishFryClock(id) {
-    clock = document.getElementById(id);
-    daysSpan = clock.querySelector('.days');
-    hoursSpan = clock.querySelector('.hours');
-    minutesSpan = clock.querySelector('.minutes');
-    secondsSpan = clock.querySelector('.seconds');
-    FishFryClock();
-    timeinterval = setInterval(FishFryClock, 1000);
-    //alert(timeinterval);
-}
-
-
-
-
-function FishFryClock() {
-    var t = getTimeRemaining(targettime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = t.hours;
-    minutesSpan.innerHTML = t.minutes;
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    if (t.total <= 0) {
-        
-        clearInterval(timeinterval);
-
+    if ((year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0))
+    {
+        return 1;
+    } else
+    {
+        return 0;
     }
 }
+//function getTimeRemaining(targettime) {
+//    var t = Date.parse(targettime) - Date.parse(new Date());
+//    alert(t);
+//    var seconds = Math.floor((t / 1000) % 60);
+//    var minutes = Math.floor((t / 1000 / 60) % 60);
+//    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//    return {
+//        'total': t,
+//        'days': days,
+//        'hours': hours,
+//        'minutes': minutes,
+//        'seconds': seconds
+//    };
+//}
+//
+//
+//
+//function initializeFishFryClock(id) {
+//    clock = document.getElementById(id);
+//    daysSpan = clock.querySelector('.days');
+//    hoursSpan = clock.querySelector('.hours');
+//    minutesSpan = clock.querySelector('.minutes');
+//    secondsSpan = clock.querySelector('.seconds');
+//    FishFryClock();
+//    timeinterval = setInterval(FishFryClock, 1000);
+//    //alert(timeinterval);
+//}
+//
+//
+//
+//
+//function FishFryClock() {
+//    var t = getTimeRemaining(targettime);
+//
+//    daysSpan.innerHTML = t.days;
+//    hoursSpan.innerHTML = t.hours;
+//    minutesSpan.innerHTML = t.minutes;
+//    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+//    if (t.total <= 0) {
+//
+//        clearInterval(timeinterval);
+//
+//    }
+//}
+//
+//function FishFryStatus(ashwednesday, easter)
+//{
+//    //alert(Date.parse(new Date()));
+//    //var offset = new Date().getTimezoneOffset() / 60;
+//    //alert(offset);
+//    //var gmt = new Date().toString().match(/([A-Z]+[\+-][0-9]+)/)[1];
+//   // alert(gmt);
+//    //window.alert(Date.parse(new Date()));
+//    // alert(ashwednesday);
+//    // alert(easter);
+//    return 0;
+//}
--- FishFryCountdown/js/CurrentYear.js
+++ FishFryCountdown/js/CurrentYear.js
@@ -4,3 +4,89 @@ function CurrentYear() {
     return n;
 
 }
+function getTimeRemaining(targettime) {
+    var t = Date.parse(targettime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+
+
+function initializeFishFryClock(id) {
+    clock = document.getElementById(id);
+    daysSpan = clock.querySelector('.days');
+    hoursSpan = clock.querySelector('.hours');
+    minutesSpan = clock.querySelector('.minutes');
+    secondsSpan = clock.querySelector('.seconds');
+    FishFryClock();
+    timeinterval = setInterval(FishFryClock, 1000);
+    //alert(timeinterval);
+}
+
+
+
+
+function FishFryClock() {
+    var t = getTimeRemaining(targettime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = t.hours;
+    minutesSpan.innerHTML = t.minutes;
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    if (t.total <= 0) {
+
+        clearInterval(timeinterval);
+
+    }
+}
+
+function FishFryStatus(targettime,ashwednesday, easter)
+{
+    var rc = 0;
+    var tt =  Date.parse(new Date());
+    //alert(tt);
+    var at = Date.parse(ashwednesday);
+    //alert(at);
+    var et = Date.parse(easter);
+
+      if (tt >= at && tt < et)
+      {
+      rc =1;
+     }
+      if (tt > et)
+     {
+      rc =2;
+     }
+     
+    return rc;
+}
+function dhms(targettime) {
+    document.getElementById("dhms").innerHTML =
+            '<div>'
+            + '    Days: <span class="days"></span><br>'
+
+            + '</div>'
+            + '<div>'
+            + '    Hours: <span class="hours"></span><br>'
+
+            + '</div>'
+            + '<div>'
+            + '    Minutes: <span class="minutes"></span> <br>'
+
+            + ' </div>'
+            + '<div>'
+            + '    Seconds: <span class="seconds"></span> <br>'
+
+            ;
+    initializeFishFryClock('FishFryClock', targettime);
+    getTimeRemaining(targettime).minutes;
+}
